6cdee9e91fd6d1eb5f3f987e8f6b0cb098f0a139
Non Madden <nonmadden@gmail.com>
2018-01-18 08:15:52 +0700

    Update all path

--- includes/class-wp-api-auth.php
+++ includes/class-wp-api-auth.php
@@ -4,14 +4,16 @@ class wp_api_auth
     protected $loader;
     protected $plugin_name;
     protected $version;
+
     public function __construct()
     {
-        $this->plugin_name = 'wp-api-auth';
+        $this->plugin_name = 'api-auth';
         $this->version = '1.0.0';
         $this->load_dependencies();
         // $this->set_locale();
         $this->define_public_hooks();
     }
+
     private function load_dependencies()
     {
         require_once plugin_dir_path(dirname(__FILE__)) . 'vendor/autoload.php';
@@ -19,6 +21,7 @@ class wp_api_auth
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-wp-api-auth-public.php';
         $this->loader = new wp_api_auth_loader();
     }
+
     private function define_public_hooks()
     {
         $plugin_public = new wp_api_auth_public($this->get_plugin_name(), $this->get_version());
@@ -27,18 +30,22 @@ class wp_api_auth
         $this->loader->add_filter('determine_current_user', $plugin_public, 'determine_current_user', 10);
         $this->loader->add_filter('rest_pre_dispatch', $plugin_public, 'rest_pre_dispatch', 10, 2);
     }
+
     public function run()
     {
         $this->loader->run();
     }
+
     public function get_plugin_name()
     {
         return $this->plugin_name;
     }
+
     public function get_loader()
     {
         return $this->loader;
     }
+
     public function get_version()
     {
         return $this->version;
